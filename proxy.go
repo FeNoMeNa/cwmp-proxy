f63c29ee7cf47e3bc9ca4258fffcdb6d40c8e784
Ivan Stefanov <ivan.stefanov@clouway.com>
2015-09-21 13:41:58 +0300

    add configuration parameters

--- proxy.go
+++ proxy.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 )
 
+var (
+	port    = flag.Int("port", 0, "CWMP proxy port")
+	backend = flag.String("backend", "", "The backend ACS server")
+)
+
 func main() {
-	p := NewProxy(8080, "http://github.com/")
-	p.Start()
+	flag.Parse()
+
+	if *port == 0 || *backend == "" {
+		flag.PrintDefaults()
+		return
+	}
+
+	log.Printf("The CWMP proxy server will be started on port %v\n", *port)
+
+	p := NewProxy(*port, *backend)
+
+	log.Fatalln(p.Start())
 }
 
 type Proxy struct {
@@ -23,11 +40,11 @@ func NewProxy(port int, backend string) *Proxy {
 	return &Proxy{port, u}
 }
 
-func (p *Proxy) Start() {
+func (p *Proxy) Start() error {
 	http.Handle("/", p.handler())
 	http.Handle("/client", wakeupHandler())
 
-	http.ListenAndServe(p.address(), nil)
+	return http.ListenAndServe(p.address(), nil)
 }
 
 func (p *Proxy) handler() http.Handler {
@@ -47,13 +64,17 @@ func wakeupHandler() http.HandlerFunc {
 		origin := r.FormValue("origin")
 
 		if origin == "" {
+			log.Println("The origin connection URL should be provided!")
 			http.Error(w, "The origin connection URL should be provided!", http.StatusBadRequest)
 			return
 		}
 
+		log.Printf("Waking up CPE with URL: %s", origin)
+
 		_, err := http.Get(origin)
 
 		if err != nil {
+			log.Printf("An error occurred with the CPE communication - %v", err)
 			http.Error(w, "An error occurred with the CPE communication!", http.StatusBadRequest)
 			return
 		}
